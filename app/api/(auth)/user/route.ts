8d56ff3c2a7d141b0d6a14f6b4e0403acd45cb2e
abhi <aasusgod@gmail.com>
2024-07-26 14:24:00 +0530

    chore: Add initial configuration files

---
+++ app/api/(auth)/user/route.ts
@@ -0,0 +1,31 @@
+import connect from '@/lib/db';
+import User from '@/lib/models/User';
+import { NextResponse } from 'next/server';
+
+export const GET = async () => {
+  try {
+    await connect();
+    const users = await User.find();
+    return new NextResponse(JSON.stringify(users), { status: 200 });
+  } catch (error: any) {
+    return new NextResponse('Error in fetching users' + error.message, {
+      status: 500,
+    });
+  }
+}
+export const POST = async (request: Request) => {
+  try {
+    const body = await request.json();
+    await connect();
+    const newUser = new User(body);
+    await newUser.save();
+    return new NextResponse(
+      JSON.stringify({ message: 'User created sucessfully', user: newUser }),
+      { status: 200 },
+    );
+  } catch (error: any) {
+    return new NextResponse('Error in creating user' + error.message, {
+      status: 500,
+    });
+  }
+};
