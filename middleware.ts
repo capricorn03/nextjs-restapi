978a798df1413ff88b54f7fa2ef065ce6c1a8aeb
abhi <aasusgod@gmail.com>
2024-07-27 07:42:09 +0530

    chore: Update middleware to handle CORS headers and preflight requests

--- middleware.ts
+++ middleware.ts
@@ -12,20 +12,32 @@ const allowedOrigins = [
 ];
 
 export default function middleware(req: Request) {
-  // retrieve the current response
+  // Retrieve the current response
   const res = NextResponse.next();
 
-  // retrieve the HTTP "Origin" header
-  // from the incoming request
-  req.headers.get('origin');
+  // Retrieve the HTTP "Origin" header from the incoming request
+  const origin = req.headers.get('origin');
 
-  // if the origin is an allowed one,
-  // add it to the 'Access-Control-Allow-Origin' header
-  if (allowedOrigins.includes(origin)) {
+  // Handle Preflight Requests (OPTIONS)
+  if (req.method === 'OPTIONS') {
+    // Add the necessary CORS headers for preflight requests
+    res.headers.append(
+      'Access-Control-Allow-Methods',
+      'GET,DELETE,PATCH,POST,PUT',
+    );
+    res.headers.append(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization',
+    ); // Add headers allowed in actual requests
+    return res; // Respond to preflight request
+  }
+
+  // Check if the origin is not null
+  if (origin !== null && allowedOrigins.includes(origin)) {
     res.headers.append('Access-Control-Allow-Origin', origin);
   }
 
-  // add the remaining CORS headers to the response
+  // Add the remaining CORS headers to the response
   res.headers.append('Access-Control-Allow-Credentials', 'true');
   res.headers.append(
     'Access-Control-Allow-Methods',
@@ -33,7 +45,7 @@ export default function middleware(req: Request) {
   );
   res.headers.append(
     'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization',
   );
 
   return res;
