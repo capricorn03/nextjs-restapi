5350f8332d53817dfa54932cfaa3bbf474a42bd9
abhi <aasusgod@gmail.com>
2024-07-27 07:48:34 +0530

    chore: Update middleware to handle CORS headers and preflight requests

--- middleware.ts
+++ middleware.ts
@@ -1,52 +1,45 @@
-import { NextResponse } from 'next/server';
-
-export const config = {
-  matcher: '/api/:path*',
-};
+import { NextRequest, NextResponse } from 'next/server';
 
 const allowedOrigins = [
   'http://localhost:8081',
   'chrome-extension://ggfdeioihiohgoomdeghoeccgajbikgd',
-  'https://example-2.com',
-  'https://example-99.com',
 ];
 
-export default function middleware(req: Request) {
-  // Retrieve the current response
-  const res = NextResponse.next();
+const corsOptions = {
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+};
 
-  // Retrieve the HTTP "Origin" header from the incoming request
-  const origin = req.headers.get('origin');
+export function middleware(request: NextRequest) {
+  // Check the origin from the request
+  const origin = request.headers.get('origin') ?? '';
+  const isAllowedOrigin = allowedOrigins.includes(origin);
 
-  // Handle Preflight Requests (OPTIONS)
-  if (req.method === 'OPTIONS') {
-    // Add the necessary CORS headers for preflight requests
-    res.headers.append(
-      'Access-Control-Allow-Methods',
-      'GET,DELETE,PATCH,POST,PUT',
-    );
-    res.headers.append(
-      'Access-Control-Allow-Headers',
-      'Content-Type, Authorization',
-    ); // Add headers allowed in actual requests
-    return res; // Respond to preflight request
+  // Handle preflighted requests
+  const isPreflight = request.method === 'OPTIONS';
+
+  if (isPreflight) {
+    const preflightHeaders = {
+      ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
+      ...corsOptions,
+    };
+    return NextResponse.json({}, { headers: preflightHeaders });
   }
 
-  // Check if the origin is not null
-  if (origin !== null && allowedOrigins.includes(origin)) {
-    res.headers.append('Access-Control-Allow-Origin', origin);
+  // Handle simple requests
+  const response = NextResponse.next();
+
+  if (isAllowedOrigin) {
+    response.headers.set('Access-Control-Allow-Origin', origin);
   }
 
-  // Add the remaining CORS headers to the response
-  res.headers.append('Access-Control-Allow-Credentials', 'true');
-  res.headers.append(
-    'Access-Control-Allow-Methods',
-    'GET,DELETE,PATCH,POST,PUT',
-  );
-  res.headers.append(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization',
-  );
+  Object.entries(corsOptions).forEach(([key, value]) => {
+    response.headers.set(key, value);
+  });
 
-  return res;
+  return response;
 }
+
+export const config = {
+  matcher: '/api/:path*',
+};
